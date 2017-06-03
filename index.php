f3658bc1f9432df7af5b30dd8a5a9089aa84b6f7
Kamaropoulos <klifeboy@live.com>
2017-06-03 18:02:00 +0300

    Added functionality to list images on the home page

--- index.php
+++ index.php
@@ -1,23 +1,188 @@
 <?php
 include_once("header.php");
 ?>
-<div class="container-fluid">
-    <div class="row">
-        <section id="pinBoot">
+<style>
+    #container {
+        width: 80%;
+        margin: auto auto;
+    }
 
-            <article class="white-panel"><img src="test.jpg" alt="">
-                <h4><a href="#">Title 1</a><i class="fa fa-thumbs-up"></i></h4>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                    irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </article>
+    .news_list {
+        list-style: none;
+    }
 
-        </section>
+    .loadmore {
+        color: #FFF;
+        border-radius: 5px;
+        width: 50%;
+        height: 50px;
+        font-size: 20px;
+        background: #42B8DD;
+        outline: 0;
+    }
 
+    .loadbutton {
+        text-align: center;
+    }
 
-        <hr>
+    #wrapper {
+        width: 90%;
+        max-width: 1100px;
+        min-width: 800px;
+        margin: 50px auto;
+    }
+
+    #columns {
+        -webkit-column-count: 3;
+        -webkit-column-gap: 10px;
+        -webkit-column-fill: auto;
+        -moz-column-count: 3;
+        -moz-column-gap: 10px;
+        -moz-column-fill: auto;
+        column-count: 3;
+        column-gap: 15px;
+        column-fill: auto;
+    }
+
+    .pin {
+        display: inline-block;
+        background: #FEFEFE;
+        border: 2px solid #FAFAFA;
+        box-shadow: 0 1px 2px rgba(34, 25, 25, 0.4);
+        margin: 0 2px 15px;
+        -webkit-column-break-inside: avoid;
+        -moz-column-break-inside: avoid;
+        column-break-inside: avoid;
+        padding: 15px;
+        padding-bottom: 5px;
+        background: -webkit-linear-gradient(45deg, #FFF, #F9F9F9);
+        opacity: 1;
+
+        -webkit-transition: all .2s ease;
+        -moz-transition: all .2s ease;
+        -o-transition: all .2s ease;
+        transition: all .2s ease;
+    }
+
+    .pin img {
+        width: 100%;
+        border-bottom: 1px solid #ccc;
+        padding-bottom: 15px;
+        margin-bottom: 5px;
+    }
+
+    .pin p {
+        font: 12px/18px Arial, sans-serif;
+        color: #333;
+        margin: 0;
+    }
+
+    @media (min-width: 960px) {
+        #columns {
+            -webkit-column-count: 4;
+            -moz-column-count: 4;
+            column-count: 4;
+        }
+    }
+
+    @media (min-width: 1100px) {
+        #columns {
+            -webkit-column-count: 5;
+            -moz-column-count: 5;
+            column-count: 5;
+        }
+    }
+
+    #columns:hover .pin:not(:hover) {
+        opacity: 0.4;
+    }
+
+    ul {
+        list-style-type: none;
+    }
+</style>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script type="text/javascript">
+    $(document).on('click', '.loadmore', function () {
+        $(this).text('Loading...');
+        var ele = $(this).parent('li');
+        $.ajax({
+            url: 'loadmore.php',
+            type: 'POST',
+            data: {
+                page: $(this).data('page'),
+            },
+            success: function (response) {
+                if (response) {
+                    ele.hide();
+                    $(".news_list").append(response);
+                }
+            }
+        });
+    });
+
+    $(document).ready(function () {
+        $("like").click(function () {
+
+            $.ajax({
+                type: 'POST',
+                url: 'like.php',
+                data: {
+
+                }
+                success: function (data) {
+                    alert(data);
+                    $("p").text(data);
+
+                }
+            });
+        });
+    });
+</script>
+<!--<div class="container-fluid">-->
+<!--    <div class="row">-->
+<!--        <section id="pinBoot">-->
+<!---->
+<!--            <article class="white-panel"><img src="test.jpg" alt="">-->
+<!--                <h4><a href="#">Title</a><i style="color: #337ab7" class="fa fa-thumbs-up pull-right"></i></h4>-->
+<!--                <p>Description</p>-->
+<!--            </article>-->
+<!---->
+<!---->
+<!--        </section>-->
+<!---->
+<!---->
+<!--        <hr>-->
+<div id="wrapper">
+    <div id="columns">
+        <ul class="news_list">
+            <?php
+            $resultsPerPage = 10;
+            $query = mysqli_query($conn, "SELECT * FROM `images` ORDER BY `images`.`pid` DESC LIMIT 0 , $resultsPerPage");
+            while ($data = mysqli_fetch_array($query)) {
+                $id = $data['pid'];
+                $title = $data['title'];
+                $content = $data['description'];
+                $image = $data['original_image'];
+                //echo "<li><h3>$title</h3><p>$content<p></li>";
+                echo "<div class='pin'>
+                      <img src='images/" . $image . "' />
+                        <h4><a href='view.php?=" . $id . "'>" . $title . "</a><i style=\"color: #337ab7\" class=\"fa fa-thumbs-up pull-right\"></i></h4>
+                        <p>" . $content . "</p>
+                      </div>";
+            }
+            ?>
+        </ul>
 
 
     </div>
+    <ul>
+        <li class="loadbutton">
+            <button class="loadmore" data-page="2">Load More</button>
+        </li>
+    </ul>
 </div>
+
 </body>
 </html>
