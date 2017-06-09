5122b35172f3964577d1444b3dece1b5173d303c
Kamaropoulos <klifeboy@live.com>
2017-06-09 22:10:31 +0300

    Added same user check

--- follow.php
+++ follow.php
@@ -6,7 +6,15 @@
         $follower_uid = $_SESSION['userid'];
         $follow_uid = mysqli_real_escape_string($conn, $_POST['id']);
 
-        $sql_check = "SELECT fid FROM follows WHERE uid_follower = " . $follower_uid . " AND followed_uid = " . $follow_uid .";";
+        if ($follower_uid == $follow_uid){
+            $sql_follows = "SELECT count(fid) as follows FROM follows WHERE followed_uid = " . $follow_uid;
+            $result = $conn->query($sql_follows);
+            $row = mysqli_fetch_assoc($result);
+            echo $row['follows'];
+            exit();
+        }
+
+        $sql_check = "SELECT fid FROM follows WHERE follower_uid = " . $follower_uid . " AND followed_uid = " . $follow_uid .";";
         $result = $conn->query($sql_check);
         if (mysqli_num_rows($result) > 0){
             // User already follow
@@ -18,10 +26,9 @@
             exit();
         } else {
             //follow user
-            $sql_follow = "INSERT INTO follows VALUES (NULL, " . $follower_uid .", ". $follow_uid .")";
+            $sql_follow = "INSERT INTO follows VALUES (NULL, " . $follower_uid . ", " . $follow_uid . ")";
             $result = $conn->query($sql_follow);
-            if (!$result){
-                echo "-1";
+            if (!$result) {
                 exit();
             }
 
@@ -33,6 +40,4 @@
             exit();
 
         }
-    } else {
-        echo "-1";
     }
