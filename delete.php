77c8914a7f687bb71bb3d2b1ff3358297f3c1537
Kamaropoulos <klifeboy@live.com>
2017-06-19 12:28:55 +0300

    Deleting a post will now also delete it's comments and the image file on the server

--- delete.php
+++ delete.php
@@ -20,13 +20,28 @@ $result = $conn->query($sql);
 if ($row = mysqli_fetch_assoc($result)) {
     if (isset($_SESSION['userid'])) {
         if ($row['uid'] == $_SESSION['userid']) {
-            $sql = "DELETE FROM images WHERE pid=" . $id;
+            $filename = "images/" . $row['original_image'];
+
+            $sql = "DELETE FROM comments WHERE pid = " . $id . ";";
 
             if ($conn->query($sql) === TRUE) {
-                header('Location:index.php');
+                $sql = "DELETE FROM images WHERE pid = " . $id . ";";
+
+                if ($conn->query($sql) === TRUE) {
+                    if (is_file($filename)) {
+                        unlink($filename);
+                    }
+                    header('Location:index.php');
+                } else {
+                    header('Location:view.php?' . $id);
+                    exit();
+                }
             } else {
                 header('Location:view.php?' . $id);
+                exit();
             }
+
+
         } else {
             header('HTTP/1.0 403 Forbidden');
             exit();
